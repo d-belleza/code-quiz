7c22eee4f54b4c3ba5a8f587c92ef041449a8bed
davidbelleza <davidbelleza@gmail.com>
2020-06-14 14:08:47 -0500

    revert to older commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,72 +11,72 @@ var scoreEl = document.getElementById("your-score");
 var saveBtnEl = document.getElementById("save-score");
 
 var questionEl = document.getElementById("question");
-var optionBtnEl = document.getElementById("option-btns")
+var btn1El = document.getElementById("btn1");
+var btn2El = document.getElementById("btn2");
+var btn3El = document.getElementById("btn3");
+var btn4El = document.getElementById("btn4");
 
 //Question assignment
 var questionArray = [
     {
-        question: "Commonly used data types DO NOT include:",
-        options: [
-            {option: "Strings", correct: false},
-            {option: "Booleans", correct: false},
-            {option: "Alerts", correct: false},
-            {option: "Numbers", correct: true}
-        ]
+        q: "Commonly used data types DO NOT include:",
+        option1: "Strings",
+        option2: "Booleans",
+        option3: "Alerts",
+        option4: "Numbers",
+        answer: "Numbers"
     },
     {
-        question: "The condition in an if/else statement is enclosed within",
-        options: [
-            {option: "Quotes", correct: false},
-            {option: "Curly Brackets", correct: true},
-            {option: "Parenthesis", correct: false},
-            {option: "Square Brackets", correct: false}
-        ]
+        q: "The condition in an if/else statement is enclosed within",
+        option1: "Quotes",
+        option2: "Curly Brackets",
+        option3: "Parenthesis",
+        option4: "Square Brackets",
+        answer: "Curly Brackets"
     },
     {
-        question: "Arrays in Javascript can be used to store",
-        options: [
-            {option: "Numbers and strings", correct: false},
-            {option: "Other Arrays", correct: false},
-            {option: "Booleans", correct: false},
-            {option: "All of the above", correct: true}
-        ]
+        q: "Arrays in Javascript can be used to store",
+        option1: "Numbers and strings",
+        option2: "Other Arrays",
+        option3: "Booleans",
+        option4: "All of the above",
+        answer: "All of the above"
     },
     {
-        question: "String values must be enclosed within____. When being assigned to variables",
-        options: [
-            {option: "Commas", correct: false},
-            {option: "Curly Brackets", correct: false},
-            {option: "Quotes", correct: false},
-            {option: "Parenthesis", correct: true}
-        ]
+        q: "String values must be enclosed within____. When being assigned to variables",
+        option1: "Commas",
+        option2: "Curly Brackets",
+        option3: "Quotes",
+        option4: "Parenthesis",
+        answer: "Parenthesis"
     },
     {
-        question: "A very useful tool used during development and debugging for printing content to the debugger is",
-        options: [
-            {option: "Javascript", correct: false},
-            {option: "Terminal/Bash", correct: false},
-            {option: "For Loops", correct: false},
-            {option: "Console.log", correct: true}
-        ]
+        q: "A very useful tool used during development and debugging for printing content to the debugger is",
+        option1: "Javascript",
+        option2: "Terminal/Bash",
+        option3: "For Loops",
+        option4: "Console.log",
+        answer: "Console.log"
     }
 ];
 
 function newScreen(){
-    endScreenEl.style.display = "none";
+    currentQuestion = 0;
+    startScreenEl.style.display = "flex";
     quizEl.style.display = "none";
+    endScreenEl.style.display = "none";
+    loadQuestion(currentQuestion);
 }
 
 function startHandler(){
-    //var timeLeft = 30;
+    var timeLeft = 10;
     startScreenEl.style.display = "none";
     quizEl.style.display = "block";
-    loadNextQuestion();
-    /*var timeInterval = setInterval(function () {
+    var timeInterval = setInterval(function () {
         timerEl.textContent = "Time: " + timeLeft + " seconds";
         timeLeft--;
-        if (timeLeft <= 0 || (currentQuestion === questionArray.length)) {
-            if(timeLeft <= 0){
+        if (timeLeft === 0 || currentQuestion === questionArray.length) {
+            if(timeLeft === 0){
                 score = 0;
             }else{
                 score = timeLeft;
@@ -85,29 +85,23 @@ function startHandler(){
             loadEndScreen();
         }
     
+      }, 1000);
+}
 
-      }, 1000);*/
+
+function loadQuestion(questionIndex){
+    var q = questionArray[questionIndex];
+    questionEl.textContent = (questionIndex + 1) + ". " + q.q;
+    btn1El.textContent = q.option1;
+    btn2El.textContent = q.option2;
+    btn3El.textContent = q.option3;
+    btn4El.textContent = q.option4;
 }
 
 function loadNextQuestion(){
-    loadQuestion(questionArray[currentQuestion]);
+        currentQuestion++;
+        loadQuestion(currentQuestion);
 }
-
-function loadQuestion(question){
-    questionEl.innerText = question.question;
-    question.options.forEach(option => {
-        var button = document.createElement('button');
-        button.innerText = options.option;
-        button.classList.add("btn");
-        if(option.correct){
-            button.dataset.correct = option.correct;
-        }
-        button.addEventListener("click", selectOption);
-        
-    });
-}
-
-
 
 function loadEndScreen(){
     quizEl.style.display = "none";
@@ -120,4 +114,7 @@ function loadEndScreen(){
 
 newScreen();
 startBtnEl.addEventListener("click", startHandler);
-
+btn1El.addEventListener("click", loadNextQuestion);
+btn2El.addEventListener("click", loadNextQuestion);
+btn3El.addEventListener("click", loadNextQuestion);
+btn4El.addEventListener("click", loadNextQuestion);
