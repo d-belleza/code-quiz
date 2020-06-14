0ae18725b8d75f25d835b9174aefc0ca96a763e9
davidbelleza <davidbelleza@gmail.com>
2020-06-14 16:32:35 -0500

    create option buttons into radio buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 //Assignments
 var currentQuestion = 0;
 var score = 0;
+var timeLeft = 30;
 
 var startScreenEl = document.getElementById("start-screen");
 var startBtnEl = document.getElementById("start-btn");
@@ -11,15 +12,17 @@ var scoreEl = document.getElementById("your-score");
 var saveBtnEl = document.getElementById("save-score");
 
 var questionEl = document.getElementById("question");
-var btn1El = document.getElementById("btn1");
-var btn2El = document.getElementById("btn2");
-var btn3El = document.getElementById("btn3");
-var btn4El = document.getElementById("btn4");
+var optn1El = document.getElementById("optn1");
+var optn2El = document.getElementById("optn2");
+var optn3El = document.getElementById("optn3");
+var optn4El = document.getElementById("optn4");
+var nextBtnEl = document.getElementById("next-btn");
+var alertEl = document.getElementById("alert-msg");
 
 //Question assignment
 var questionArray = [
     {
-        q: "Commonly used data types DO NOT include:",
+        question: "Commonly used data types DO NOT include:",
         option1: "Strings",
         option2: "Booleans",
         option3: "Alerts",
@@ -27,7 +30,7 @@ var questionArray = [
         answer: "Numbers"
     },
     {
-        q: "The condition in an if/else statement is enclosed within",
+        question: "The condition in an if/else statement is enclosed within",
         option1: "Quotes",
         option2: "Curly Brackets",
         option3: "Parenthesis",
@@ -35,7 +38,7 @@ var questionArray = [
         answer: "Curly Brackets"
     },
     {
-        q: "Arrays in Javascript can be used to store",
+        question: "Arrays in Javascript can be used to store",
         option1: "Numbers and strings",
         option2: "Other Arrays",
         option3: "Booleans",
@@ -43,7 +46,7 @@ var questionArray = [
         answer: "All of the above"
     },
     {
-        q: "String values must be enclosed within____. When being assigned to variables",
+        question: "String values must be enclosed within____. When being assigned to variables",
         option1: "Commas",
         option2: "Curly Brackets",
         option3: "Quotes",
@@ -51,7 +54,7 @@ var questionArray = [
         answer: "Parenthesis"
     },
     {
-        q: "A very useful tool used during development and debugging for printing content to the debugger is",
+        question: "A very useful tool used during development and debugging for printing content to the debugger is",
         option1: "Javascript",
         option2: "Terminal/Bash",
         option3: "For Loops",
@@ -69,17 +72,17 @@ function newScreen(){
 }
 
 function startHandler(){
-    var timeLeft = 10;
+    var countdown = timeLeft;
     startScreenEl.style.display = "none";
     quizEl.style.display = "block";
     var timeInterval = setInterval(function () {
-        timerEl.textContent = "Time: " + timeLeft + " seconds";
-        timeLeft--;
-        if (timeLeft === 0 || currentQuestion === questionArray.length) {
-            if(timeLeft === 0){
+        timerEl.textContent = "Time: " + countdown + " seconds";
+        countdown--;
+        if (countdown === 0 || currentQuestion === questionArray.length) {
+            if(countdown === 0){
                 score = 0;
             }else{
-                score = timeLeft;
+                score = countdown;
             }
             clearInterval(timeInterval);
             loadEndScreen();
@@ -91,16 +94,34 @@ function startHandler(){
 
 function loadQuestion(questionIndex){
     var q = questionArray[questionIndex];
-    questionEl.textContent = (questionIndex + 1) + ". " + q.q;
-    btn1El.textContent = q.option1;
-    btn2El.textContent = q.option2;
-    btn3El.textContent = q.option3;
-    btn4El.textContent = q.option4;
+    questionEl.textContent = (questionIndex + 1) + ". " + q.question;
+    optn1El.textContent = q.option1;
+    optn2El.textContent = q.option2;
+    optn3El.textContent = q.option3;
+    optn4El.textContent = q.option4;
 }
 
 function loadNextQuestion(){
-        currentQuestion++;
-        loadQuestion(currentQuestion);
+    var selectedOption = document.querySelector("input[type=radio]:checked");
+    if (!selectedOption) {
+        alert("Please select your answer!");
+        return;
+
+    }
+    var answer = selectedOption.nodeValue;
+    if(questionArray[currentQuestion].answer == answer) {
+        alertEl.textContent = "Correct!";
+    }
+    selectedOption.checked = false;
+    currentQuestion++;
+    if(currentQuestion === (questionArray.length - 1)) {
+        nextBtnEl.textContent = "Finish";
+
+    }
+    if(currentQuestion === questionArray.length){
+        loadEndScreen();
+    }
+    loadQuestion(currentQuestion);
 }
 
 function loadEndScreen(){
@@ -108,13 +129,10 @@ function loadEndScreen(){
     endScreenEl.style.display = "flex";
     scoreEl.textContent = "Score: " + score;
     timerEl.textContent = "";
-    saveBtnEl.addEventListener("click", newScreen)
+    saveBtnEl.addEventListener("click", newScreen);
 
 }
 
 newScreen();
 startBtnEl.addEventListener("click", startHandler);
-btn1El.addEventListener("click", loadNextQuestion);
-btn2El.addEventListener("click", loadNextQuestion);
-btn3El.addEventListener("click", loadNextQuestion);
-btn4El.addEventListener("click", loadNextQuestion);
+nextBtnEl.addEventListener("click", loadNextQuestion);
